f89b1aabf587a84fd59711da7dfe732d4138e069
Vikram-04 <vikramrawal7893@gmail.com>
2026-07-24 13:15:37 +0530

    Listen on port provided by Render environment

--- Practice-app-backend/index.js
+++ Practice-app-backend/index.js
@@ -97,7 +97,7 @@ app.put("/api/notes/:id", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
