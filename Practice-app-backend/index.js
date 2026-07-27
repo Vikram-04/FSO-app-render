9ba828727b0d13ff9ee5380ffc86f83674b26559
Vikram-04 <vikramrawal7893@gmail.com>
2026-07-28 00:07:09 +0530

    error-handling

--- Practice-app-backend/index.js
+++ Practice-app-backend/index.js
@@ -21,21 +21,25 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    if (!note) {
-      return response
-        .status(404)
-        .json({ error: `note with id ${request.params.id} does not exist` });
-    }
-    response.json(note);
-  });
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response
+          .status(404)
+          .json({ error: `note with id ${request.params.id} does not exist` });
+      }
+      response.json(note);
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((n) => n.id !== id);
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/notes", (request, response) => {
@@ -53,23 +57,37 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
-app.put("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  const body = request.body;
-  if (!body.content || body.important === undefined) {
-    response
-      .status(400)
-      .json({ error: `Updated note must contain content and important field` });
-    return;
-  }
-  note = {
-    id: note.id,
-    content: body.content,
-    important: body.important,
-  };
-  notes = notes.map((n) => (n.id === id ? note : n));
-  response.json(note);
+app.put("/api/notes/:id", (request, response, next) => {
+  const { content, important } = request.body;
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (!note) {
+        return response.status(404).end();
+      }
+      note.content = content;
+      note.important = important;
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote);
+      });
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).json({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
